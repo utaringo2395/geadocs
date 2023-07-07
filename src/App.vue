626f6b85fd29aaaf9e6e31d2a73dc1897f37e674
Mariana M <mariana.macias2395@gmail.com>
2023-07-07 15:59:21 -0600

    Update

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,10 @@ import navguide from './components/navguide.vue'
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-lg-2 px-0" style="border: 1px solid red;">
+      <div class="col-lg-2 px-0" style="background: #212529;">
         <navguide/>
       </div>
-      <div class="col-lg-10 px-0" style="border: 1px solid blue;">
+      <div class="col-lg-10 px-0" style="height: 100vh; background: #fff;">
         <RouterView />
       </div>
     </div>
